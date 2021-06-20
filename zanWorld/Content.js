8d90956c445f82310fbfd42a83563881c3f056a4
ZanZubair96 <zanzubair2@gmail.com>
2021-06-21 00:45:09 +0530

    edited message

--- zanWorld/Content.js
+++ zanWorld/Content.js
@@ -1,6 +1,6 @@
 const headers = document.getElementsByTagName("h2");
 
 for(let i=0; i < headers.length; i++){
-    headers[i].innerText = "Zan is awesome"; 
+    headers[i].innerText = "Zan is Great"; 
 }
 
